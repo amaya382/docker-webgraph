b7652e9957379facf89e1ab0339f1fdd0613235f
amaya <git@sapphire.in.net>
2017-01-06 21:39:52 +0900

    [MOD] typo

--- get_edgelist.sh
+++ get_edgelist.sh
@@ -3,5 +3,5 @@
 [[ -e $1.properties ]] || wget http://data.law.di.unimi.it/webdata/$1/$1.properties
 [[ -e $1.graph ]] || wget http://data.law.di.unimi.it/webdata/$1/$1.graph
 
-sed -e "s/graphname/$1/g" docker-compose.yml > $1_docker-conpose.yml
-docker-compose -f $1_docker-conpose.yml up
+sed -e "s/graphname/$1/g" docker-compose.yml > $1_docker-compose.yml
+docker-compose -f $1_docker-compose.yml up
